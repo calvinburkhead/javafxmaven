454bbadc3d977f4c5b7ccf8ee579d99a5bf577e0
Calvin Burkhead <calvin@nwmsrocks.com>
2017-08-24 18:20:20 -0700

    Initial Commit

---
+++ src/main/java/com/nwmsrocks/javafxmaven/Main.java
@@ -0,0 +1,30 @@
+package com.nwmsrocks.javafxmaven;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Uploader.fxml"));
+			Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
